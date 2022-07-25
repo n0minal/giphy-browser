f828f1123caf16a4ee48c83d73eaf090be4ec695
Leonardo Moura <leo@trend.io>
2022-07-24 22:29:44 -0300

    feat: adds trending giphy fetch route, adds offset params to support pagination of gifs

--- src/api/giphy.js
+++ src/api/giphy.js
@@ -1,11 +1,30 @@
 import axios from 'axios';
 
-export const searchGiphy = async (searchTerm, limit=10) => {
+export const searchGiphy = async (searchTerm, offset = 0, limit = 10) => {
   try {
     const { data } = await axios.get(`https://api.giphy.com/v1/gifs/search?q=${searchTerm}`, {
       params: {
         api_key: process.env.REACT_APP_GIPHY_API_KEY,
+        offset,
+        limit
+      }
+    });
+
+    return data.data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export const searchTrendingGiphy = async (offset = 0, limit = 10) => {
+  try {
+    const { data } = await axios.get(`https://api.giphy.com/v1/gifs/trending`, {
+      params: {
+        api_key: process.env.REACT_APP_GIPHY_API_KEY,
+        offset,
         limit,
+        rating: 'g'
       }
     });
 
