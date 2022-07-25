e53b9950e4ffcbd3b37c9aa9734e8611cad08908
Leonardo Moura <leo@trend.io>
2022-07-24 22:03:04 -0300

    feat: adds basic search functionality, adds new giphycard component details

--- src/components/GiphyCard.js
+++ src/components/GiphyCard.js
@@ -1,10 +1,28 @@
-import { Card } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 
-const GiphyCard = ({ }) => {
+const GiphyCard = () => {
   return (
-    <Card>
-      <img src="https://media1.giphy.com/media/kgUzxTu7TMa9d4UXcJ/giphy.gif?cid=bb5a1c3agbc0wa6209q18y4fxiclq2s7h7hrigwm51pdju6q&rid=giphy.gif"/>
-    </Card>
+    <Card sx={{ minWidth: 275 }}>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        Lorem
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </Card>
   );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Input, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { searchGiphy } from '../api/giphy';
 
 const HomePage = () => {
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = async () => {
+    const result = await searchGiphy(searchTerm, 10);
+    console.log(result);
+  }
+
   return (
     <div>
       <h1>Giphy Browser</h1>
-      <Input placeholder='Search for a gif' />
-      <Button>Search</Button>
-      <Button>
+      <Input placeholder='Search for a gif' onChange={e => setSearchTerm(e.target.value)}/>
+      <Button variant="contained" onClick={handleSearch}>Search</Button>
+      <Button variant="text">
         <Link to="/my-saved-gifs">My Saved Gifs</Link>
       </Button>
     </div>
